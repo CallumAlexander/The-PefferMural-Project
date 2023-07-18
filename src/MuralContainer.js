81987494a52c4b76e11565146fd124657626ae67
Cal Alexander <46627336+callumalexander@users.noreply.github.com>
2023-07-18 19:47:47 +0100

    18/07 beginning of day. MuralContainer.js that doesn't work.

--- src/MuralContainer.js
+++ src/MuralContainer.js
@@ -1,11 +1,37 @@
+import React from 'react';
+import OpenSeadragon from 'openseadragon';
 
 
-function MuralContainer() {
+const MuralContainer = () => {
+
+    // Define image source and other display props
+    const imageURL  = 'mural.jpg';
+    const initial_zoom_level = '';
+
+    // Event handle for zoom changes
+    const HandleZoomChanges = (zoom_level) => {
+        // Update zoom level state or perform other actions
+        console.log('Zoom level changed:', zoom_level);
+    };
+
+    // Event handle for panning
+    const HandlePanChanges = (event) => {
+      // Perform any necessary actions based on panning
+        console.log('Pan event:', event);
+    };
+
     return (
-        <div>
+    <div className="mural-container">
+      <OpenSeadragon
+        image={imageURL}
+        initialZoom={initial_zoom_level}
+        onZoomChange={HandleZoomChanges}
+        onPan={HandlePanChanges}
+      />
+    </div>
+  );
 
-        </div>
-    );
+
 }
 
 export default MuralContainer;
