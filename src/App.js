b830a44fa08185c6eb071cda3e173247b1441ee0
Cal Alexander <46627336+callumalexander@users.noreply.github.com>
2023-07-20 21:36:36 +0100

    Added swiper.js to the project and implemented a basic carousell into the app that can be built upon.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg'
 import MuralContainer from "./MuralContainer";
 import './App.css';
+import Carousell from "./Carousell";
 
 function App() {
   return (
-    <div className="App">
-      <MuralContainer />
-    </div>
+      <div className="App">
+          {/*<MuralContainer />*/}
+          <Carousell/>
+      </div>
   );
 }
 
