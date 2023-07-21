f77aff6d3d11604316920005f4ea7de9dcdb40d1
Cal Alexander <46627336+callumalexander@users.noreply.github.com>
2023-07-21 13:49:06 +0100

    FreeMode works now

--- src/Carousell.js
+++ src/Carousell.js
@@ -13,22 +13,24 @@ import {FreeMode, Pagination} from 'swiper/modules';
 const Carousell = () => {
     return (
         <Swiper
-            spaceBetween={50}
             slidesPerView={3}
-            freemode={true}
+            spaceBetween={30}
+            freeMode={true}
             pagination={{
                 clickable: true,
             }}
             modules={[FreeMode, Pagination]}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
+            className="mySwiper"
         >
             <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide sigma</SwiperSlide>
             <SwiperSlide>Slide 2</SwiperSlide>
             <SwiperSlide>Slide 3</SwiperSlide>
             <SwiperSlide>Slide 4</SwiperSlide>
-            ...
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
         </Swiper>
     );
 };
