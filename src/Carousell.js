16d9a46a29b5d3602853dc76a41611b7f163b86c
Cal Alexander <46627336+callumalexander@users.noreply.github.com>
2023-07-21 10:52:33 +0100

    Kinda got freemode imported but I am not sure that it is working.

--- src/Carousell.js
+++ src/Carousell.js
@@ -1,14 +1,25 @@
 // Import Swiper React components
 import {Swiper, SwiperSlide} from 'swiper/react';
 
+
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+// Import required Swiper Modules
+import {FreeMode, Pagination} from 'swiper/modules';
 
 const Carousell = () => {
     return (
         <Swiper
             spaceBetween={50}
             slidesPerView={3}
+            freemode={true}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[FreeMode, Pagination]}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
         >
